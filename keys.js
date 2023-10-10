74da3655e7337bfd3de04d082e4ea40082540e5d
Chethana <chethanasv22@gmail.com>
2023-10-10 15:00:34 +0530

    updated keys and pairs function

--- keys.js
+++ keys.js
@@ -1,4 +1,13 @@
-export function keys(obj) {
-    return Object.keys(obj);
+export function keys(obj){
+    let resultArr = [];
+    for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+            resultArr.push(key);
+        }
+    }
+    return resultArr;
 }
 
+// export function keys(obj){
+//     return Object.keys(obj);
+// }
--- pairs.js
+++ pairs.js
@@ -1,6 +1,17 @@
-export function pairs(obj) {
-    return Object.entries(obj);
+export function pairs(obj){
+    let resultArray = [];
+    for(let key in obj){
+        resultArray.push([key, obj[key]]);
+    }
+    return resultArray;
 }
+
+// export function pairs(obj) {
+//     return Object.entries(obj);
+// }
+
+
+
 
 
 
